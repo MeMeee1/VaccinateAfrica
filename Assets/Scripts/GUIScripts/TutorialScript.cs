63a37760f9b04c5e8ae662dfe642b5bc512508e1
estheroe <epelleoesther@gmail.com>
2022-04-08 00:37:24 +0100

    Just Finished all the scen

---
+++ Assets/Scripts/GUIScripts/TutorialScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] pic;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(index >=5)
+        {
+            index = 5;
+        }
+        if(index <0)
+        {
+            index = 0;
+        }
+
+        if(index ==0)
+        {
+            pic[0].gameObject.SetActive(true);
+        }
+         
+    }
+
+
+    public void Next()
+    {
+
+        index += 1;
+        for (int i=0; i<pic.Length;i++)
+        {
+
+            pic[i].gameObject.SetActive(false);
+            pic[index].gameObject.SetActive(true);
+        }
+
+    }
+    public void Back()
+    {
+
+        index -= 1;
+        for (int i = 0; i < pic.Length; i++)
+        {
+
+            pic[i].gameObject.SetActive(false);
+            pic[index].gameObject.SetActive(true);
+        }
+
+    }
+
+
+}

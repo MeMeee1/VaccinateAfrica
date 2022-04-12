a4720e26a2dea2e8ac93169eb3e42847d8e28375
estheroe <epelleoesther@gmail.com>
2022-04-12 23:06:22 +0100

    just testing

---
+++ VaccinateAfrica/Assets/Scripts/GUIScripts/Tutorial.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] GameObject[] pic;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+    }
+    [SerializeField] GameObject button = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if(index >=5)
+        {
+            index = 5;
+            button.SetActive(false);
+        }
+        if(index <0)
+        {
+            index = 0;
+            button.SetActive(true);
+        }
+
+        if(index ==0)
+        {
+            pic[0].gameObject.SetActive(true);
+            button.SetActive(true);
+        }
+
+        if(index >0 && index <5)
+        {
+            button.SetActive(true);
+        }
+         
+    }
+
+
+    public void Next()
+    {
+
+        index += 1;
+        for (int i=0; i<=pic.Length-1;i++)
+        {
+
+            pic[i].gameObject.SetActive(false);
+            pic[index].gameObject.SetActive(true);
+        }
+
+
+    }
+    public void Back()
+    {
+
+        index -= 1;
+        for (int i = 0; i < pic.Length; i++)
+        {
+
+            pic[i].gameObject.SetActive(false);
+            pic[index].gameObject.SetActive(true);
+        }
+
+    }
+}
